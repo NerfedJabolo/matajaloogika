f1e08db5907881285f9fe30b1fc409e0797ec51e
Sten <sten.kahu@voco.ee>
2022-10-19 10:01:41 +0300

    mathematical expressions.

---
+++ math_expressions.py
@@ -0,0 +1,26 @@
+'''Practicing with different mathematical expressions and calculations.'''
+
+#declare num_a and num_b
+
+num_a = 3
+num_b = 5
+
+# 1. Sum and difference
+
+sum = num_a + num_b
+print(f"{num_a} + {num_b} = {sum}")
+
+difference = num_a - num_b
+print(f"{num_a} - {num_b} = {difference}")
+
+print('------------------------------------')
+
+# 2. Float division
+
+division = num_a / num_b;
+print(f"{num_a} / {num_b} = {division}")
+
+# 3. Integer division
+
+division = num_a // num_b;
+print(f"{num_a} // {num_b} = {division}")
